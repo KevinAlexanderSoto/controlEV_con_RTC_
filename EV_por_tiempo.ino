bbf68d14f1fff90991aab88ba09b5fd7541418ee
Kevin Soto <kevinalexandersoto999@gmail.com>
2021-08-10 11:15:53 -0500

    work on controlvalve and hour

--- EV_por_tiempo.ino
+++ EV_por_tiempo.ino
@@ -1,29 +1,33 @@
-#include "RTC_EXPORT.h"
-
-#define valve1 8 
-#define valve2 7
-#define h_anterior GET_HOUR()// intervlao de riego , 1 es por hora , 2 es media hora etc...
-
-void setup() {
-  Serial.begin(9600);    // inicializa comunicacion serie a 9600 bps
-  pinMode(valve1,OUTPUT);
-  pinMode(valve2,OUTPUT);
- INICIAR_MODULO ();
-}
-
-void loop() {
+  #include "RTC_EXPORT.h"
   
-ControlValve();
-
-
-delay(10000);
-}
-
-void ControlValve(){
-int h_actual = GET_HOUR();
-
-if(h_actual != h_anterior)
-// TODO: encender valvular por un intervalo de tiempo 
-h_anterior=h_actual;
-}
-
+  #define valve1 8 
+  #define valve2 7
+  
+  int h_anterior = 1;//GET_HOUR();
+  int h_actual ;
+  
+  void setup() {
+      Serial.begin(9600); 
+      Serial.println("serial init");// inicializa comunicacion serie a 9600 bps
+      pinMode(valve1,OUTPUT);
+      pinMode(valve2,OUTPUT);
+      INICIAR_MODULO ();
+  }
+  
+  void loop() {
+      Serial.println("estoy en loop2");
+    //ControlValve();
+    
+    
+    delay(1000);
+  }
+  
+  void ControlValve(){
+     h_actual = GET_HOUR();
+     
+    if(h_actual != h_anterior){
+    // TODO: encender valvular por un intervalo de tiempo 
+    h_anterior = h_actual;
+    }
+    
+  }
