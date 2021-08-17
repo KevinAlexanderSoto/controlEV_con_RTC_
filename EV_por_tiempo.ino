12515f36c28b98b84f9824ea63c13b04fec6f3ed
Kevin Soto <kevinalexandersoto999@gmail.com>
2021-08-17 08:01:53 -0500

    first commit second version , interval variable

--- EV_por_tiempo.ino
+++ EV_por_tiempo.ino
@@ -3,8 +3,10 @@
   #define valve1 8 
   #define valve2 7
 
-  int h_anterior ;
+  int h_activado  ;
   int h_actual ;
+
+  int intervalo_Activado = 2;//brecha de activacion de las valvulas
   
   void setup() {
       Serial.begin(9600); 
@@ -12,7 +14,7 @@
       pinMode(valve1,OUTPUT);
       pinMode(valve2,OUTPUT);
       INICIAR_MODULO ();// inicia el RTC con fecha de compilacion y setea otras opciones 
-       h_anterior = GET_MINUTE();
+      h_activado = GET_MINUTE()+ intervalo_Activado;
   }
   void loop() {
     ControlValve();
@@ -27,9 +29,14 @@
   }
   
   void ControlValve(){
+    
+    if(h_activado > 60){// para caso especial de usar minutos, con hora es 24
+      h_activado = h_activado - 60;
+      }
+      
      h_actual = GET_MINUTE();
      
-    if(h_actual != h_anterior){
+    if(h_actual >= h_activado){
     Serial.println("VALVE1 ON");
     digitalWrite(valve1, HIGH);
   
@@ -48,7 +55,7 @@
 
     // TODO: activar segunda salida "valve2"
     
-    h_anterior = h_actual;// para volver a comparar y hacer el ciclo 
+    h_activado = GET_MINUTE()+ intervalo_Activado;// para volver a comparar y hacer el ciclo 
     }
     
   }
